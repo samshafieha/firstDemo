8694ec3bcfdfa89f133dff6d7310ea6d68c59a67
Yaqub Hasan <Yhasan3@ivc.edu>
2022-10-11 20:18:42 -0700

    Added Functions

--- main.cpp
+++ main.cpp
@@ -2,8 +2,47 @@
 
 using namespace std;
 
+int addNum()
+{
+	int num;
+
+	num = 5+4;
+
+	return num;
+}
+
+int subtractNum()
+{
+	int num;
+
+	num = 5-4;
+
+	return num;
+}
+
+int multiplyNum()
+{
+	int num;
+
+	num = 5*4;
+
+	return num;
+}
+
 int main(){
+	int num, num2, num3;
 	cout << "HI GIT";
+	cout << "Hello Git";
+
+	num = addNum();
+	num2 = subtractNum();
+	num3 = multiplyNum();
+	
+
+	cout << "Add the numbers " << num << endl;
+	cout << "Subtract the numbers " << num << endl;
+	cout << "Multiply the numbers " << num << endl;
+
 	
 	return 0;
 }

54940e0e75693a03782bd7a9cfab70b00d68b680
tholm1 <tholm1@saddleback.edu>
2022-10-12 17:26:25 -0700

    Added multiplying

--- Calculator.cpp
+++ Calculator.cpp
@@ -11,6 +11,7 @@ using namespace std;
 
 void adding(int num1, int num2);
 void subtracting(int num1, int num2);
+void multiplying(int num1, int num2);
 
 int main()
 {
@@ -19,6 +20,7 @@ int main()
     
     adding(num1, num2);
     subtracting(num1, num2);
+	multiplying(num1, num2);
     
     return 0;
 }
@@ -33,4 +35,10 @@ void subtracting(int num1, int num2)
 {
     int sum = num1 - num2;
     cout << sum << endl;
+}
+
+void multiplying(int num1, int num2)
+{
+    int num3 = num1 * num2;
+    cout << num3 << endl;
 }

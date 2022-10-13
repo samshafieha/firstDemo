041556d49a33a769c88b6d13ac45162b7a7fdccc
tholm1 <tholm1@saddleback.edu>
2022-10-12 17:18:31 -0700

    Initial Commit

---
+++ main copy.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+using namespace std;
+
+int addNum()
+{
+	int num;
+
+	num = 5+4;
+
+	return num;
+}
+
+int subtractNum()
+{
+	int num;
+
+	num = 5-4;
+
+	return num;
+}
+
+int multiplyNum()
+{
+	int num;
+
+	num = 5*4;
+
+	return num;
+}
+
+void sayHi()
+{
+	cout << "hi";
+}
+
+int main(){
+	int num, num2, num3;
+	cout << "HI GIT";
+	cout << "\nHello Git";
+
+
+	num = addNum();
+	num2 = subtractNum();
+	num3 = multiplyNum();
+	
+
+	cout << "\nAdd the numbers " << num;
+	cout << "\nSubtract the numbers " << num2;
+	cout << "\nMultiply the numbers " << num3;
+	
+	return 0;
+}
